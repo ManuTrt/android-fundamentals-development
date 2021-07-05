0811c008a0b47f0acdf2cec26ab0abbe799e472d
ManuTrt <emanuelthegamer@gmail.com>
2021-07-05 17:21:20 +0300

    done challange 1

--- _4_view_groups2/challenge_1/app/src/main/java/com/adg/challenge_1/MainActivity.java
+++ _4_view_groups2/challenge_1/app/src/main/java/com/adg/challenge_1/MainActivity.java
@@ -1,14 +1,93 @@
 package com.adg.challenge_1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.adg.challenge_1.adapter.ContactListAdapter;
+import com.adg.challenge_1.model.Contact;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView contactList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get the reference to the RV
+        contactList = findViewById(R.id.actMain_listContacts);
+
+        // set the Layout Manager for the RV
+        // using LinearLayout will order the items one by one
+        RecyclerView.LayoutManager llm = new LinearLayoutManager(this);
+        contactList.setLayoutManager(llm);
+
+        // create a dummy data source
+        ArrayList<Contact> contactListDS = new ArrayList<>();
+        contactListDS.add(new Contact("Ana", "0333111111", R.drawable.ic_launcher_foreground));
+        contactListDS.add(new Contact("Ada", "0333222111", R.drawable.ic_launcher_foreground));
+        contactListDS.add(new Contact("Bogdan", "0333333111", R.drawable.ic_launcher_foreground));
+        contactListDS.add(new Contact("Dan", "0333444111", R.drawable.ic_launcher_foreground));
+        contactListDS.add(new Contact("Dumitru", "0333555111", R.drawable.ic_launcher_foreground));
+        contactListDS.add(new Contact("George", "0333666111", R.drawable.ic_launcher_foreground));
+        contactListDS.add(new Contact("Zamfir", "0333777111", R.drawable.ic_launcher_foreground));
+        contactListDS.add(new Contact("Ana2", "0333111111", R.drawable.ic_launcher_foreground));
+        contactListDS.add(new Contact("Ada2", "0333222111", R.drawable.ic_launcher_foreground));
+        contactListDS.add(new Contact("Bogdan2", "0333333111", R.drawable.ic_launcher_foreground));
+        contactListDS.add(new Contact("Dan2", "0333444111", R.drawable.ic_launcher_foreground));
+        contactListDS.add(new Contact("Dumitru2", "0333555111", R.drawable.ic_launcher_foreground));
+        contactListDS.add(new Contact("George2", "0333666111", R.drawable.ic_launcher_foreground));
+        contactListDS.add(new Contact("Zamfir2", "0333777111", R.drawable.ic_launcher_foreground));
+        contactListDS.add(new Contact("Ana3", "0333111111", R.drawable.ic_launcher_foreground));
+        contactListDS.add(new Contact("Ada3", "0333222111", R.drawable.ic_launcher_foreground));
+        contactListDS.add(new Contact("Bogdan3", "0333333111", R.drawable.ic_launcher_foreground));
+        contactListDS.add(new Contact("Dan3", "0333444111", R.drawable.ic_launcher_foreground));
+        contactListDS.add(new Contact("Dumitru3", "0333555111", R.drawable.ic_launcher_foreground));
+        contactListDS.add(new Contact("George3", "0333666111", R.drawable.ic_launcher_foreground));
+        contactListDS.add(new Contact("Zamfir3", "0333777111", R.drawable.ic_launcher_foreground));
+
+        contactListDS.sort((o1, o2) -> o1.getName().compareTo(o2.getName()));
+
+        ArrayList<Contact> contactListDSWithHeaders = new ArrayList<>();
+
+        // adding headers
+        if (contactListDS.size() > 0) {
+            int i = 0;
+            char lastChar = contactListDS.get(0).getName().charAt(0);
+
+            contactListDSWithHeaders.add(i, new Contact(String.valueOf(lastChar), "x", null));
+            i++;
+
+            for (int j = 0; j < contactListDS.size(); j++) {
+                char currentChar = contactListDS.get(j).getName().charAt(0);
+
+                if (currentChar != lastChar) {
+                    lastChar = currentChar;
+                    contactListDSWithHeaders.add(i, new Contact(String.valueOf(lastChar), "x", null));
+                    i++;
+                }
+
+                contactListDSWithHeaders.add(i, contactListDS.get(j));
+                i++;
+            }
+        }
+
+        // create adapter for the data source
+        ContactListAdapter adapter = new ContactListAdapter(contactListDSWithHeaders);
+
+        // set the adapter to the RV
+        contactList.setAdapter(adapter);
     }
 }
--- _4_view_groups2/challenge_1/app/src/main/java/com/adg/challenge_1/adapter/ContactListAdapter.java
+++ _4_view_groups2/challenge_1/app/src/main/java/com/adg/challenge_1/adapter/ContactListAdapter.java
@@ -1,11 +1,13 @@
 package com.adg.challenge_1.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
+import androidx.annotation.*;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.adg.challenge_1.R;
@@ -13,44 +15,91 @@ import com.adg.challenge_1.model.Contact;
 
 import java.util.ArrayList;
 
-public class ContactListAdapter extends RecyclerView.Adapter<ContactListAdapter.ContactViewHolder>
+public class ContactListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
 {
-    private ArrayList<Contact> mDataSource;
+    private static final int TYPE_CONTACT = 0;
+    private static final int TYPE_HEADER = 1;
 
-    public ContactListAdapter(ArrayList<Contact> mDataSource) {
-        this.mDataSource = mDataSource;
+    private ArrayList<Contact> contacts;
+
+    public ContactListAdapter (ArrayList<Contact> contacts) {
+        this.contacts = contacts;
     }
 
     @NonNull
     @Override
-    public ContactViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        RelativeLayout view;
+
+        if (viewType == TYPE_CONTACT) {
+            // Transform the XML into a java object, without adding the child view to the parent right away (attachToRoot = false)
+            // LayoutManager will handle the operation of adding the child to its parent so that's why it's false
+            view = (RelativeLayout) LayoutInflater.from(parent.getContext()).inflate(R.layout.item_contact, parent, false);
+
+            return new ContactListViewHolder(view);
+        } else {
+            view = (RelativeLayout) LayoutInflater.from(parent.getContext()).inflate(R.layout.item_header, parent, false);
+
+            return new HeaderViewHolder(view);
+        }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ContactViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof HeaderViewHolder) {
+            ((HeaderViewHolder) holder).update(position);
+        } else {
+            ((ContactListViewHolder) holder).update(position);
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return contacts.size();
     }
 
-    public class ContactViewHolder extends RecyclerView.ViewHolder
-    {
-        private ImageView mContactIcon;
-        private TextView mContactName;
-        private TextView mContactPhoneNo;
+    @Override
+    public int getItemViewType(int position) {
+        if (contacts.get(position).getPhoneNumber().equals("x"))
+            return TYPE_HEADER;
+        return TYPE_CONTACT;
+    }
 
-        public ContactViewHolder(@NonNull View itemView) {
+    public class ContactListViewHolder extends RecyclerView.ViewHolder
+    {
+        private ImageView contactIcon;
+        private TextView contactName;
+        private TextView contactPhoneNumber;
+
+        public ContactListViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            mContactIcon = itemView.findViewById(R.id.itemContact_conImg);
-            mContactName = itemView.findViewById(R.id.itemContact_conName);
-            mContactPhoneNo = itemView.findViewById(R.id.itemContact_conPhoneNo);
+            contactIcon = itemView.findViewById(R.id.itemContact_conImg);
+            contactName = itemView.findViewById(R.id.itemContact_conName);
+            contactPhoneNumber = itemView.findViewById(R.id.itemContact_conPhoneNo);
         }
 
+        public void update(int position) {
+            Contact contact = contacts.get(position);
 
+            contactIcon.setImageResource(contact.getIconType());
+            contactName.setText(contact.getName());
+            contactPhoneNumber.setText(contact.getPhoneNumber());
+        }
+    }
+
+    public class HeaderViewHolder extends RecyclerView.ViewHolder
+    {
+        private TextView headerLetter;
+
+        public HeaderViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            headerLetter = itemView.findViewById(R.id.itemHeader_header);
+        }
+
+        public void update(int position) {
+            headerLetter.setText(contacts.get(position).getName());
+        }
     }
 }
