badc7fd634ea259c1d5656cb7252c52c1d206194
ManuTrt <emanuelthegamer@gmail.com>
2021-07-08 13:45:16 +0100

    c8 added more practice

--- _8_networking/app/src/main/java/com/adg/networking/JsonPlaceHolderApi.java
+++ _8_networking/app/src/main/java/com/adg/networking/JsonPlaceHolderApi.java
@@ -1,15 +1,29 @@
 package com.adg.networking;
 
+import com.adg.networking.models.Comment;
 import com.adg.networking.models.Post;
 
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.Field;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.PATCH;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
+import retrofit2.http.Url;
 
 /*
  * Here we have an interface meant to
- * represent the api for the JPH;
+ * represent the api for the JPH REST API SERVER;
  * It contains the definitions of the
  * methods that will later be implemented
  * by Retrofit (annotations will help
@@ -22,8 +36,122 @@ import retrofit2.http.GET;
 public interface JsonPlaceHolderApi {
     /*
      * 'posts' is the relative url to
-     * the endpoint that the posts are at
+     * the endpoint that the posts are at;
      */
     @GET("posts")
     Call<List<Post>> getPosts();
+
+    /*
+     * A possible endpoint may look like
+     * this '/posts?userId=5' which allows
+     * us to query the information based
+     * on some conditions
+     */
+    @GET("posts")
+    Call<List<Post>> getPosts(@Query("userId") int userId);
+
+    /*
+     * Also, multiple Query parameters are
+     * supported '/posts?userId=5&_sort=id&_order=desc'
+     */
+    @GET("posts")
+    Call<List<Post>> getPosts(
+            @Query("userId") int userId,
+            @Query("_sort") String sort,
+            @Query("_order") String order
+    );
+
+    /*
+     * For more flexibility in choosing the
+     * query parameters we can use QueryMap
+     * where the key is the query parameter
+     * and the value will be the actual value
+     */
+    @GET("posts")
+    Call<List<Post>> getPosts(@QueryMap Map<String, String> parameters);
+
+    /*
+     * In order not to hardcode the values
+     * necessary for the GET request,
+     * a replacement block is mandatory
+     * using curly braces;
+     * It needs to be passed as an argument
+     * to the method
+     */
+    @GET("posts/{id}/comments")
+    Call<List<Comment>> getComments(@Path("id") int postId);
+
+    /*
+     * The most generic method is to pass the url
+     * for the get request at runtime
+     */
+    @GET
+    Call<List<Comment>> getComments(@Url String url);
+
+    /*
+     * In case we want to send data
+     * to the server, a POST request
+     * is sent having a Body that contains
+     * our information;
+     * GSON will convert the data from our
+     * post object to the json equivalent;
+     * Given the fact that a fake rest api
+     * is used, the post won't actually be stored
+     * on the server and it will fake its completion
+     * (sending back a successful HTTP Response with
+     * the whole post we sent)
+     */
+    @POST("posts")
+    Call<Post> createPost(@Body Post post);
+
+    /*
+     * A more flexible method is using
+     * a FormUrlEncoded. This is usually used
+     * in html forms or simple key value
+     * pairs format, not nested ones.
+     * Url encoding means that the
+     * body will contain '&' between the
+     * key-value pairs and also '%20'
+     * instead of spaces
+     */
+    @FormUrlEncoded
+    @POST("posts")
+    Call<Post> createPost(
+            @Field("userId") int userId,
+            @Field("title") String title,
+            @Field("body") String text
+    );
+
+    @FormUrlEncoded
+    @POST("posts")
+    Call<Post> createPost(@FieldMap Map<String, String> fields);
+
+    /*
+     * Updating a single post on the server
+     * using a placeholder for the post id
+     * and a Body that will contain the new
+     * values we want to change the contents of
+     * the post with
+     */
+    @PUT("posts/{id}")
+    Call<Post> putPost(@Path("id") int id, @Body Post post);
+
+    /*
+     * Analogous to PUT is the PATCH request.
+     * PUT will change the post completely with
+     * what we sent over.
+     * PATCH will change only the fields we
+     * specify (not null ones). GSON by default
+     * ignores null values so the resulting
+     * json wont contain any field that previously
+     * was initialized as null;
+     */
+    @PATCH("posts/{id}")
+    Call<Post> patchPost(@Path("id") int id, @Body Post post);
+
+    /*
+     * Deleting a post
+     */
+    @DELETE("posts/{id}")
+    Call<Void> deletePost(@Path("id") int id);
 }
--- _8_networking/app/src/main/java/com/adg/networking/MainActivity.java
+++ _8_networking/app/src/main/java/com/adg/networking/MainActivity.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.adg.networking.models.Comment;
 import com.adg.networking.models.Post;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -21,6 +24,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textViewResult;
+    private JsonPlaceHolderApi jsonPlaceHolderApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,14 +33,31 @@ public class MainActivity extends AppCompatActivity {
 
         textViewResult = findViewById(R.id.actMain_text_view);
 
-
+        /*
+         * In case the server hosts data in another format
+         * such as XML, another converter shall be used
+         */
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://jsonplaceholder.typicode.com/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
-        Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
+        jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+
+        //getPosts();
+        //getComments();
+        //createPost();
+        //updatePost();
+        deletePost();
+    }
+
+    private void getPosts() {
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("userId", "1");
+        parameters.put("_sort", "id");
+        parameters.put("_order", "desc");
+
+        Call<List<Post>> call = jsonPlaceHolderApi.getPosts(parameters);
 
         /* 'enqueue' will send the request on a different thread
          * (async) and the methods will run on the UI thread */
@@ -63,6 +84,125 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<List<Post>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+
+    private void getComments() {
+        Call<List<Comment>> call = jsonPlaceHolderApi.getComments("posts/3/comments");
+
+        call.enqueue(new Callback<List<Comment>>() {
+            @Override
+            public void onResponse(Call<List<Comment>> call, Response<List<Comment>> response) {
+                if(!response.isSuccessful()) {
+                    textViewResult.setText("Code: " + response.code());
+                    return;
+                }
+
+                List<Comment> comments = response.body();
+
+                for (Comment comment : comments) {
+                    String content = "";
+                    content += "ID: " + comment.getId() + "\n";
+                    content += "Post ID: " + comment.getPostId() + "\n";
+                    content += "Name: " + comment.getName() + "\n";
+                    content += "Email: " + comment.getEmail() + "\n";
+                    content += "Text: " + comment.getText() + "\n\n";
+
+                    textViewResult.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Comment>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+
+    private void createPost() {
+        Post post = new Post(23, "New Title", "New Text");
+
+        //Call<Post> call = jsonPlaceHolderApi.createPost(post);
+        //Call<Post> call = jsonPlaceHolderApi.createPost(23, "New Title", "New Text");
+        Map<String, String> fields = new HashMap<>();
+        fields.put("userId", "25");
+        fields.put("title", "New Title");
+
+        Call<Post> call = jsonPlaceHolderApi.createPost(fields);
+
+        call.enqueue(new Callback<Post>() {
+            @Override
+            public void onResponse(Call<Post> call, Response<Post> response) {
+                if(!response.isSuccessful()) {
+                    textViewResult.setText("Code: " + response.code());
+                    return;
+                }
+
+                Post postResponse = response.body();
+
+                String content = "";
+                content += "Code: " + response.code() + "\n";
+                content += "ID: " + postResponse.getId() + "\n";
+                content += "User ID: " + postResponse.getUserId() + "\n";
+                content += "Title: " + postResponse.getTitle() + "\n";
+                content += "Text: " + postResponse.getText() + "\n\n";
+
+                textViewResult.setText(content);
+            }
+
+            @Override
+            public void onFailure(Call<Post> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+
+    private void updatePost() {
+        Post post = new Post(12, null, "New Text");
+
+        //Call<Post> call = jsonPlaceHolderApi.putPost(5, post);
+        Call<Post> call = jsonPlaceHolderApi.patchPost(5, post);
+
+        call.enqueue(new Callback<Post>() {
+            @Override
+            public void onResponse(Call<Post> call, Response<Post> response) {
+                if(!response.isSuccessful()) {
+                    textViewResult.setText("Code: " + response.code());
+                    return;
+                }
+
+                Post postResponse = response.body();
+
+                String content = "";
+                content += "Code: " + response.code() + "\n";
+                content += "ID: " + postResponse.getId() + "\n";
+                content += "User ID: " + postResponse.getUserId() + "\n";
+                content += "Title: " + postResponse.getTitle() + "\n";
+                content += "Text: " + postResponse.getText() + "\n\n";
+
+                textViewResult.setText(content);
+            }
+
+            @Override
+            public void onFailure(Call<Post> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+
+    private void deletePost() {
+        Call<Void> call = jsonPlaceHolderApi.deletePost(5);
+
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                textViewResult.setText("Code: " + response.code());
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
                 textViewResult.setText(t.getMessage());
             }
         });
